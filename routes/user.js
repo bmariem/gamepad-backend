42aa0c9ed564501d502e74ac22985e221af54f87
bmariem <mariem.borchani@gmail.com>
2022-03-08 09:51:04 +0100

    Handle adding & removing favorite game from user's collection

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const axios = require("../config/api-axios");
 
 const uid2 = require("uid2");
 const SHA256 = require("crypto-js/sha256");
@@ -61,8 +62,6 @@ router.post("/signup", async (req, res) => {
 
         await newUser.save(); // save newUser on DataBase
 
-        console.log(newUser);
-
         res.status(200).json({
           _id: newUser._id,
           email: newUser.email,
@@ -122,6 +121,106 @@ router.post("/login", async (req, res) => {
   } catch (error) {
     console.log(error.message);
     res.json({ message: error.message });
+  }
+});
+
+// User's collection
+router.get("/user/collections", isAuthenticated, async (req, res) => {
+  try {
+    res.status(200).json({
+      id: req.user._id,
+      email: req.user.email,
+      username: req.user.username,
+      avatar: req.user.avatar,
+      favorites: {
+        favoriteGames: req.user.favorites.favoriteGames,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: error.message,
+    });
+  }
+});
+
+const addFavoriteGames = (user, game) => {
+  if (user.favorites === undefined) {
+    user.favorites = {
+      favoriteGames: [],
+    };
+  }
+
+  // check if game already exists in favorites
+  let exist = user.favorites.favoriteGames.find((element) => {
+    return element.id === game.id;
+  });
+
+  // game does not exists in favorites <=> add it
+  if (!exist) {
+    user.favorites.favoriteGames.push(game);
+  }
+};
+
+// Add User's fav to collection
+router.post("/user/collections", isAuthenticated, async (req, res) => {
+  try {
+    // #swagger.parameters['User id'] = { description: 'Insert user ID.', type: 'string' , required: true }
+    const user = await User.findById(req.user._id); // Checking if user ID is already in my bd
+
+    // #swagger.parameters['Game id'] = { description: 'Insert game ID.', type: 'string' , required: true }
+    const response = await axios.get(
+      `/games/${req.fields.id}?key=${process.env.API_KEY}`
+    );
+
+    addFavoriteGames(user, response.data);
+    await user.save();
+
+    res.status(200).json(req.user);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      message: error.message,
+    });
+  }
+});
+
+const deleteFavoriteGames = (user, gameId) => {
+  if (user.favorites === undefined) {
+    user.favorites = {
+      favoriteGames: [],
+    };
+  }
+
+  // check if game already exists in favorites
+  let existingFav = user.favorites.favoriteGames.filter((element) => {
+    return element.id === Number(gameId);
+  });
+
+  // game exists in favorites <=> delete it
+  if (existingFav.length > 0) {
+    const index = user.favorites.favoriteGames.indexOf(existingFav[0]);
+    if (index > -1) {
+      user.favorites.favoriteGames.splice(index, 1); // 2nd parameter means remove one item only
+    }
+  }
+};
+
+// Delete User's fav from collection
+router.delete("/user/collections/:id", isAuthenticated, async (req, res) => {
+  try {
+    // #swagger.parameters['User id'] = { description: 'Insert user ID.', type: 'string' , required: true }
+    const user = await User.findById(req.user._id); // Checking if user ID is already in my bd
+
+    // #swagger.parameters['gameId'] = { description: 'Insert the ID of the game.', type: 'string' , required: true }
+    const gameId = req.params.id;
+
+    deleteFavoriteGames(user, gameId);
+    await user.save();
+    res.status(200).json({ message: "Game deleted from favorite collection" });
+  } catch (error) {
+    res.status(400).json({
+      message: error.message,
+    });
   }
 });
 
